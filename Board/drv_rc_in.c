d0f633c4660baa43015650a7a4eecb930bcde1e4
wirano <982593395@qq.com>
2021-04-30 17:26:01 +0800

    fix ppm input edge detect

--- Board/drv_rc_in.c
+++ Board/drv_rc_in.c
@@ -23,10 +23,10 @@ void PPM_IRQH()
     //
     if (TIM3->SR & TIM_IT_CC1)
     {
-        TIM3->SR = ~TIM_IT_CC1;
-        TIM3->SR = ~TIM_FLAG_CC1OF;
+        TIM3->SR &= ~TIM_IT_CC1;
+        TIM3->SR &= ~TIM_FLAG_CC1OF;
         //==
-        if (!(GPIOB->IDR & GPIO_PIN_4))
+        if (GPIOB->IDR & GPIO_PIN_4)
         {
             temp_cnt[0] = TIM3->CCR1;
         }
