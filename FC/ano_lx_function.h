41c17f30a9148df36383907683fc094526f736a4
wirano <982593395@qq.com>
2021-05-01 22:04:18 +0800

    add api declare

--- FC/ano_lx_function.h
+++ FC/ano_lx_function.h
@@ -13,6 +13,8 @@ uint8_t FC_Lock(void);
 
 uint8_t LX_Change_Mode(uint8_t new_mode);
 
+uint8_t OneKey_Hover(void);
+
 uint8_t OneKey_Takeoff(uint16_t height_cm);
 
 uint8_t OneKey_Land(void);
@@ -21,9 +23,17 @@ uint8_t OneKey_Flip(void);
 
 uint8_t OneKey_Return_Home(void);
 
+uint8_t Vertical_Rising(uint16_t height_cm, uint16_t velocity_cmps); //0-10000cm 10-300cm/s
+
+uint8_t Vertical_Declining(uint16_t height_cm, uint16_t velocity_cmps);
+
 uint8_t Horizontal_Calibrate(void);
 
 uint8_t Horizontal_Move(uint16_t distance_cm, uint16_t velocity_cmps, uint16_t dir_angle_0_360);
+
+uint8_t Left_Rotate(uint16_t deg,uint16_t dps); //deg:0-359 dps:5-90deg/s
+
+uint8_t Right_Rotate(uint16_t deg,uint16_t dps); //deg:0-359 dps:5-90deg/s
 
 uint8_t Mag_Calibrate(void);
 
