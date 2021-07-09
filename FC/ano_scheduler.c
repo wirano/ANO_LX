84e93717057ab0bebc609a8dd3cc7f9cb5aa4302
GrayGrayLiu <2984940004@qq.com>
2021-07-09 16:20:54 +0800

    合并tof

--- FC/ano_scheduler.c
+++ FC/ano_scheduler.c
@@ -49,12 +49,8 @@ static void Loop_50Hz(void) //20ms执行一次
 {
     //////////////////////////////////////////////////////////////////////
     user_rgb_tasks(20);
-    process_control();
     omv_data_analysis(omv.rec_buffer_p,omv.rec_len);
     omv_offline_check(20);
-    process_delay(&Takeoff_delay);
-    process_delay(&Unlock_delay);
-    process_delay(&Block_delay);
     //////////////////////////////////////////////////////////////////////
 }
 
